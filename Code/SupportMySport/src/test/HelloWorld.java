e27e7b788f7ab5b3a82a51fd13a77f6e45262789
Jotschgl <johannes.schwendinger@hotmail.com>
2012-10-24 15:02:27 +0200

    Hello World

---
+++ Code/SupportMySport/src/test/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Johannes
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+    }
+}
