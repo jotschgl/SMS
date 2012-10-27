291dde2ce75ea030d9f3fff6cca878bb5ce43cf9
Dennis <Dennis@172.16.49.136>
2012-10-27 19:40:18 +0200

    Persistenz Schicht erstellt inklusive Manger, welcher das speicher, updaten und löschen von Daten übernimmt.

---
+++ Code/SupportMySport/src/Testing/TestController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Testing;
+
+import Persistence.PersistenceManager;
+import Persistence.Right;
+import Persistence.Role;
+import Persistence.Rule;
+import Persistence.Sport;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ *
+ * @author Dennis
+ */
+public class TestController {
+    
+    public static void main(String args[]){
+    }
+    
+}
