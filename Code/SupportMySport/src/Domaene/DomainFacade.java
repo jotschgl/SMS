7e8bc275d399267811cf0feebf3fe3860a940c92
rafjack <rafael.jakob.neumann@gmail.com>
2012-11-02 20:02:25 +0100

    Started with competition manager

--- Code/SupportMySport/src/Domaene/DomainFacade.java
+++ Code/SupportMySport/src/Domaene/DomainFacade.java
@@ -4,15 +4,17 @@
  */
 package Domaene;
 
+import java.util.Date;
 import java.util.List;
 
 /**
  *
  * @author Dennis
  */
-public class DomainFacade {
+public class DomainFacade { 
 
     private ClubMemberManager clubMemberManager;
+    private CompetitionManager competitionManager;
 
     public DomainFacade() {
         clubMemberManager = new ClubMemberManager();
@@ -23,5 +25,46 @@ public class DomainFacade {
     }
     
     
-
+    // <editor-fold defaultstate="collapsed" desc="Competition Specific Calls">
+    
+    public Competition getCompetitionById(int id){
+        //TODO: get all Competitions from the database
+        return competitionManager.getCompetitinById(id);
+    }
+    
+    public Competition getCompetitionByName(String name){
+        //TODO: get Competitions from the database with a specific name
+        return null;
+    }
+    
+    public List<Competition> getCompetitionsByName(String name){
+        //TODO: get Competitions from the database with a specific name
+        return null;
+    }
+    
+    public Competition getCompetitionByDepartment(int departmentId){
+        //TODO: get the Competition connected with an department
+        return null;
+    }
+    
+    public Competition getCompetitionByDate(Date date){
+        //TODO: get a Competition at a specific date
+        return null;
+    }
+    
+    public List<Competition> getAllCompetitionsByDate(Date startdate){
+        //TODO: get all competitions from startdate
+        return null;
+    }
+    
+    public List<Competition> getAllCompetitionsBetweenPeriod(Date startdate, Date endDate){
+         //TODO: get all Competitions between two Dates
+        return null;
+    }
+    
+    public List<Competition> getAllCompetitions(){
+        //TODO: get all Competitions from the database
+        return null;
+    }
+    // </editor-fold>
 }
