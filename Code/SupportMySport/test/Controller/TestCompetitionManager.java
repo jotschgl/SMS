e003a1088cabb26668a5ad7b70943733b1e8dfb6
rafjack <rafael.jakob.neumann@gmail.com>
2012-11-02 20:08:31 +0100

    Changed the package of the competition test

--- Code/SupportMySport/test/Controller/TestCompetitionManager.java
+++ Code/SupportMySport/test/Controller/TestCompetitionManager.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package TestFacadeSubsystems;
+package Controller;
 
 
 import Domaene.CompetitionManager;
