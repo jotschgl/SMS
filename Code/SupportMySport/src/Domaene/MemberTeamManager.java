3c3c1d5237902cddaf27a819ef2efb3a7efb54d7
rafjack <rafael.jakob.neumann@gmail.com>
2012-11-03 19:08:42 +0100

    Added new class MemberTeamManager

---
+++ Code/SupportMySport/src/Domaene/MemberTeamManager.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Domaene;
+
+import Persistence.ClubMember;
+import Persistence.Meeting;
+import Persistence.PersistenceManager;
+import Persistence.Team;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author rafa
+ */
+public class MemberTeamManager {
+
+    public List<ClubMember> getMembersOfTeams(List<Team> teams) {
+        
+        //TODO: Missing the MemberTeam class in persistence
+        //PersistenceManager persistenceManager = new PersistenceManager();
+        //List<MemberTeam> memberTeam = new LinkedList<MemberTeam>();
+        //for(Team team : teams){
+            
+        //}
+        //return competitionMeetings
+        return null;
+    }
+}
