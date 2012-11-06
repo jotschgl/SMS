e809915c0fb58a1b130edf776e659ffee4de86af
Andreas Lorenz <lorenz.and@gmail.com>
2012-11-06 16:32:22 +0100

    UPDATE- ANDI

---
+++ Code/SupportMySport_Client/src/GUI/GUIController.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GUI;
+
+import Communication.ClubMemberDTO;
+import Communication.interfaces.IUseCaseControllerFactory;
+import Controller.interfaces.IClubMemberController;
+import Controller.interfaces.ICompetitionController;
+import java.rmi.AccessException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Andreas
+ */
+public class GUIController {
+
+    private static IUseCaseControllerFactory factory;
+    private static ICompetitionController competController;
+    private static IClubMemberController memberController;
+
+    public static void initRMI(String ip) {
+        if (factory == null) {
+            try {
+                Registry reg = LocateRegistry.getRegistry(ip);
+                factory = (IUseCaseControllerFactory) reg.lookup("UseCaseControllerFactory");
+                
+                competController = factory.createCompetitionController();
+                memberController = factory.createClubMemberController();
+
+            } catch (NotBoundException ex) {
+                Logger.getLogger(GUIController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (AccessException ex) {
+                Logger.getLogger(GUIController.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (RemoteException ex) {
+                Logger.getLogger(GUIController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    public static IClubMemberController getClubMemberController() {
+        return memberController;
+    }
+}
