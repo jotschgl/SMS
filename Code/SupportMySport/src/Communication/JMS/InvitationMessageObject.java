0cb067697d5718a79c168fbd23bc8fd206fdb8d3
jotschgl <johannes.schwendinger@hotmail.com>
2012-11-26 15:33:45 +0100

    Bam

--- Code/SupportMySport/src/Communication/JMS/InvitationMessageObject.java
+++ Code/SupportMySport/src/Communication/JMS/InvitationMessageObject.java
@@ -1,26 +1,31 @@
 package Communication.JMS;
 
+import MessageInterfaces.IInvitationMessage;
 import java.io.Serializable;
 
-public class InvitationMessageObject implements Serializable{
+public class InvitationMessageObject implements Serializable, IInvitationMessage{
         
         String subject;
         String competitionDate;
         String competitionName;
         String messageBody;
 
+    @Override
         public String getSubject() {
             return subject;
         }
 
+    @Override
         public String getCompetitionDate() {
             return competitionDate;
         }
 
+    @Override
         public String getCompetitionName() {
             return competitionName;
         }
 
+    @Override
         public String getMessageBody() {
             return messageBody;
         }
---
+++ Code/SupportMySportInterfaces/src/MessageInterfaces/IInvitationMessage.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MessageInterfaces;
+
+/**
+ *
+ * @author Johannes
+ */
+public interface IInvitationMessage {
+
+    public String getCompetitionDate();
+
+    public String getCompetitionName();
+
+    public String getMessageBody();
+
+    public String getSubject();
+    
+}
