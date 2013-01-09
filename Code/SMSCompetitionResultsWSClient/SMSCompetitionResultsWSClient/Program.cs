6902cfe198e26d59f021f2ba637ea1671d1b9b3c
rafjack <rafael.jakob.neumann@gmail.com>
2013-01-09 10:12:55 +0100

    Added new web service client

---
+++ Code/SMSCompetitionResultsWSClient/SMSCompetitionResultsWSClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMSCompetitionResultsWSClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //"Liga 1", "Fussball", "2013-12-11"
+
+            String liga = "";
+            String sport = "";
+            String datum = "";
+
+            CompetitionResultsWS.CompetitionsResultsWSClient client = new CompetitionResultsWS.CompetitionsResultsWSClient();
+
+            while (true) {
+                Console.Write("Please enter Liga:");
+                liga = Console.ReadLine();
+                Console.Write("Please enter Sport:");
+                sport = Console.ReadLine();
+                Console.Write("Please enter date:");
+                datum = Console.ReadLine();
+
+                Console.WriteLine();
+
+                Console.Write("Competition results:");
+                Console.WriteLine(client.getCompetitionResults(liga, sport, datum));
+                Console.Write("Competition status:");
+                Console.WriteLine(client.isCompetitionCompleted(liga, sport, datum));
+                Console.WriteLine();
+            }
+
+
+           
+        }
+    }
+}
